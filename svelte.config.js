2b8948e84b71937adbeb482674577bba55792886
Abdullah Reghvie <reghvieabdullah@gmail.com>
2025-10-06 17:39:28 +0530

    did some random config shit

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
 import netlify from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: netlify(),
-		// optional: you can specify build options if needed
-		// target: '#svelte', // if using an older SvelteKit version
 	}
 };
 
