60fb9c022cea87fd0460b960905def9c6cfeccab
Abdullah Reghvie <reghvieabdullah@gmail.com>
2025-10-06 17:53:36 +0530

    fk this

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-	kit: {
-		adapter: adapter({
-			split: false,       // optional, prevents Edge Functions split
-			redirects: undefined,
-			headers: undefined
-		})
-	}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	kit: { adapter: adapter() }
 };
+
+export default config;
