ea68dfc4d6349b7344c0724d82bbfc071662ce7e
Abdullah Reghvie <reghvieabdullah@gmail.com>
2026-09-03 16:50:54 +0530

    added contact us page. added api, added function to insert

---
+++ src/routes/api/v1/contact/+server.ts
@@ -0,0 +1,12 @@
+import { json, type RequestHandler } from "@sveltejs/kit";
+import { contactRegardingPotentialCustomers } from "$lib/dbFunctions/venuesDB";
+
+
+export const POST: RequestHandler = async ({ locals, request }) => {
+    const body = await request.json().catch(() => null);
+    if (!body) return json({ error: "Invalid JSON" }, { status: 400 });
+    const { contactName, contactEmail, contactPhone } = body;
+    const result = await contactRegardingPotentialCustomers(locals.supabase, contactName, contactEmail, contactPhone);
+    if (result.error) return json({ error: result.error }, { status: 400 });
+    return json(result.data, { status: 200 });
+};
