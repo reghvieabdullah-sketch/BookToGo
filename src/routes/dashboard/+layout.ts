720d95095ebc7414fd508c4e46c8abd989b17e8b
Abdullah Reghvie <reghvieabdullah@gmail.com>
2026-09-04 06:12:29 +0530

    changed startDate to today, since when nobody needs bookings before today, and endDate reflects maxBookingNoticeMinutes

--- src/routes/dashboard/+layout.ts
+++ src/routes/dashboard/+layout.ts
@@ -11,8 +11,10 @@ export const load: LayoutLoad = async ({ fetch, parent, depends, url }) => {
   const dateParam = url.searchParams.get(QUERY_PARAM_BOOKING_DATE);
   const targetDate = dateParam ? new Date(dateParam) : new Date();
   
-  const startDate = new Date(targetDate.getFullYear(), targetDate.getMonth(), 1);
-  const endDate = new Date(targetDate.getFullYear(), targetDate.getMonth() + 2, 0);
+  const startDate = new Date();
+  const allowedBookingDaysIntoFuture = parentData.settingsData.maxBookingNoticeMinutes; // convert minutes to days
+  const endDate = new Date(startDate.getTime() + allowedBookingDaysIntoFuture * 60 * 1000); // add allowed days into future to start date
+  // const endDate = new Date(targetDate.getFullYear(), targetDate.getMonth() + 2, 0);
 
   const response = await fetch(
     `/api/v1/bundler/${venueID}?${QUERY_PARAM_VENUE_BOOKING_DATE_START}=${startDate.toISOString()}&${QUERY_PARAM_VENUE_BOOKING_DATE_END}=${endDate.toISOString()}&${QUERY_PARAM_BOOKING_CLOSURE_BUNDLE_DASHBOARD_TYPE}=true`
