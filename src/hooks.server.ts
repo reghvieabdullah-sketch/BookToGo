c7815cf22618dfabb77f19c4cfc690f2a24af791
Abdullah Reghvie <reghvieabdullah@gmail.com>
2026-09-04 18:58:44 +0530

    made it a cool color, and removed stmarybball

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -21,7 +21,7 @@ function getVenueURL(hostname: string): string | null {
 }
 
 const supabase: Handle = async ({ event, resolve }) => {
-    const host = event.request.headers.get('host') ?? 'stmarybball';
+    const host = event.request.headers.get('host') ?? '';
     // const subdomain = 'stjohnsbb'; // PLEASE REMEMBER TO NEVER HAVE THIS HARDCODED
     event.locals.venueURL = getVenueURL(host) ;
     
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -19,7 +19,7 @@ function getVenueURL(hostname: string): string | null {
 }
 
 export const load: LayoutLoad = async ({ data, depends, fetch, url }) => {
-  const venueURL = getVenueURL(url.hostname) ?? 'stmarybball';
+  const venueURL = getVenueURL(url.hostname) ?? '';
   depends('supabase:auth');
   depends(INVALIDATE_PARENT_LAYOUT_CODE);
 
