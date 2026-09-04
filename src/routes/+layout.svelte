85add49a1aebf86cfee79e74f2f7b4dd0e7a6290
Abdullah Reghvie <reghvieabdullah@gmail.com>
2026-09-04 08:00:45 +0530

    Try again, for bookign thing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	// import favicon from '$lib/assets/favicon.svg';
 	import { onMount } from 'svelte';
 	import { goto, invalidate } from '$app/navigation';
 
